03769731f0b4391516a42f1098efc25090623e88
KateKate <anishkina@gmail.com>
2022-08-08 16:33:23 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  const now = performance.now();
+  while (performance.now() - now < 100) {}
+
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <h1 className={styles.title}>Index</h1>
+
+      <div className={styles.grid}>
+        <p className={styles.card}>
+          <Link href="/page1">Go to Page1</Link>
+        </p>
+      </div>
+    </>
+  );
+}
